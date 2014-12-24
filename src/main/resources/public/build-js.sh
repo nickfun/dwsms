4ee8178064328b3cdabaf54f13d8b517e2af8c15
Nicholas Funnell <nick@nick.gs>
2014-12-24 09:52:41 -0800

    playing with yui-compressor

--- src/main/resources/public/build-js.sh
+++ src/main/resources/public/build-js.sh
@@ -1,1 +1,9 @@
-yui-compressor -o ../vendors.js jquery.js underscore.js backbone.js backbone.babysitter.js backbone.wreqr.js backbone.marionette.js
+#!/bin/sh
+
+echo Begin
+yui-compressor -o vendors.min.js \
+    backbone.marionette/jquery.js \
+    backbone.marionette/underscore.js \
+    backbone.marionette/backbone.js \
+    backbone.marionette/backbone.marionette.js 
+echo Done! Check vendors.min.js
