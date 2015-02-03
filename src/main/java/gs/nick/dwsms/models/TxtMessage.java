f5046be750e19175e46719103c6894f8dab1db9c
Nick Funnell <nick@nick.gs>
2015-02-02 17:05:57 -0800

    adding propper unit tests

--- src/main/java/gs/nick/dwsms/models/TxtMessage.java
+++ src/main/java/gs/nick/dwsms/models/TxtMessage.java
@@ -1,5 +1,6 @@
 package gs.nick.dwsms.models;
 
+import java.util.Objects;
 import org.joda.time.LocalDateTime;
 
 /**
@@ -20,4 +21,39 @@ public class TxtMessage {
                 && body != null
                 && body.length() <= 160;
     }
+
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		hash = 47 * hash + Objects.hashCode(this.to);
+		hash = 47 * hash + Objects.hashCode(this.from);
+		hash = 47 * hash + Objects.hashCode(this.body);
+		hash = 47 * hash + Objects.hashCode(this.send);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final TxtMessage other = (TxtMessage) obj;
+		if (!Objects.equals(this.to, other.to)) {
+			return false;
+		}
+		if (!Objects.equals(this.from, other.from)) {
+			return false;
+		}
+		if (!Objects.equals(this.body, other.body)) {
+			return false;
+		}
+		if (!Objects.equals(this.send, other.send)) {
+			return false;
+		}
+		return true;
+	}
+	
 }
---
+++ src/test/java/gs/nick/dwsms/models/MessageDatabaseTest.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gs.nick.dwsms.models;
+
+import java.util.Iterator;
+import org.joda.time.LocalDateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nickf
+ */
+public class MessageDatabaseTest {
+
+	public MessageDatabaseTest() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of add method, of class MessageDatabase.
+	 */
+	@Test
+	public void testAdd() throws Exception {
+		System.out.println("add");
+		TxtMessage msg = new TxtMessage();
+		msg.body = "test1";
+		msg.from = "from";
+		msg.to = "test1";
+		msg.send = LocalDateTime.now();
+		MessageDatabase instance = new MessageDatabase(3);
+		assertEquals(0, instance.size());
+		instance.add(msg);
+		assertEquals(1, instance.size());
+		assertEquals(msg, instance.pop(LocalDateTime.now().plusDays(100)));
+	}
+
+	/**
+	 * Test of pop method, of class MessageDatabase.
+	 */
+	@Test
+	public void testPop() throws Exception {
+		System.out.println("pop");
+		TxtMessage msg = new TxtMessage();
+		msg.body = "test1";
+		msg.from = "from";
+		msg.to = "test1";
+		msg.send = LocalDateTime.now();
+		MessageDatabase instance = new MessageDatabase(3);
+		assertEquals(0, instance.size());
+		instance.add(msg);
+		assertEquals(1, instance.size());
+		assertEquals(msg, instance.pop(LocalDateTime.now().plusDays(100)));
+	}
+
+	/**
+	 * Test of size method, of class MessageDatabase.
+	 */
+	@Test
+	public void testSize() throws Exception {
+		System.out.println("add");
+		TxtMessage msg = new TxtMessage();
+		msg.body = "test1";
+		msg.from = "from";
+		msg.to = "test1";
+		msg.send = LocalDateTime.now();
+		MessageDatabase instance = new MessageDatabase(3);
+		assertEquals(0, instance.size());
+		instance.add(msg);
+		assertEquals(1, instance.size());
+		assertEquals(msg, instance.pop(LocalDateTime.now().plusDays(100)));
+	}
+
+	/**
+	 * Test of clear method, of class MessageDatabase.
+	 */
+	@Test
+	public void testClear() throws Exception {
+		System.out.println("clear");
+		TxtMessage msg = new TxtMessage();
+		msg.body = "test1";
+		msg.from = "from";
+		msg.to = "test1";
+		msg.send = LocalDateTime.now();
+		MessageDatabase instance = new MessageDatabase(3);
+		assertEquals(0, instance.size());
+		instance.add(msg);
+		assertEquals(1, instance.size());
+		instance.clear();
+		assertEquals(0, instance.size());
+	}
+
+}
